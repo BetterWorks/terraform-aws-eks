c445eb3aebc801bad08450cdcc507b32090ec524
henryhe <henry.he@betterworks.com>
2020-07-15 13:01:56 -0600

    simplifying outputs

--- modules/iam-service-account/outputs.tf
+++ modules/iam-service-account/outputs.tf
@@ -1,15 +1,15 @@
  
 output "this_iam_role_arn" {
   description = "ARN of IAM role"
-  value       = element(concat(aws_iam_role.this.*.arn, [""]), 0)
+  value       = aws_iam_role.this.arn
 }
 
 output "this_iam_role_name" {
   description = "Name of IAM role"
-  value       = element(concat(aws_iam_role.this.*.name, [""]), 0)
+  value       = aws_iam_role.this.name
 }
 
 output "this_iam_role_path" {
   description = "Path of IAM role"
-  value       = element(concat(aws_iam_role.this.*.path, [""]), 0)
+  value       = aws_iam_role.this.name
 }
