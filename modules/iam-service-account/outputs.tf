9d85b2fdd4a9339c3eca23463bf1fc769ebd1b05
henryhe <henry.he@betterworks.com>
2020-07-15 13:03:42 -0600

    limiting

--- modules/iam-service-account/outputs.tf
+++ modules/iam-service-account/outputs.tf
@@ -1,15 +1,15 @@
  
-output "this_iam_role_arn" {
+output "iam_role_arn" {
   description = "ARN of IAM role"
   value       = aws_iam_role.this.arn
 }
 
-output "this_iam_role_name" {
+output "iam_role_name" {
   description = "Name of IAM role"
   value       = aws_iam_role.this.name
 }
 
-output "this_iam_role_path" {
+output "iam_role_path" {
   description = "Path of IAM role"
   value       = aws_iam_role.this.name
 }
