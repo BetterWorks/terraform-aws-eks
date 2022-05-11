856f5370ff64b8399ddfc8c7fa303f31db846a5c
Henry He <henry.he@betterworks.com>
2022-05-11 12:40:56 -0600

    adding Iam roles

--- modules/node_groups/locals.tf
+++ modules/node_groups/locals.tf
@@ -1,6 +1,9 @@
 locals {
   # Merge defaults and per-group values to make code cleaner
   node_groups_expanded = { for k, v in var.node_groups : k => merge(
+    {
+      iam_role_arn                  = var.default_iam_role_arn
+    },
     var.node_groups_defaults,
     v,
   ) if var.create_eks }
