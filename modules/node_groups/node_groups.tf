98e4286550601f55db3369ba93eade7a5e0de658
Henry He <henry.he@betterworks.com>
2023-01-31 16:23:02 -0700

    putting changes back in

--- modules/node_groups/node_groups.tf
+++ modules/node_groups/node_groups.tf
@@ -44,6 +44,7 @@ resource "aws_eks_node_group" "workers" {
   }
   lifecycle {
     create_before_destroy = true
+    ignore_changes        = [scaling_config.0.desired_size]
   }
 
   depends_on = [aws_launch_template.workers]
