9efa5f35f7d77202aa4894da3b1af40796dd8a3c
Justin Ross <justin.ross@betterworks.com>
2025-06-24 10:30:25 -0600

    chore: pin to 5.x aws provider

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.0"
 
   required_providers {
-    aws        = "~> 5.13.0"
+    aws        = "~> 5.0"
     local      = ">= 1.2"
     null       = ">= 2.1"
     template   = ">= 2.1"
